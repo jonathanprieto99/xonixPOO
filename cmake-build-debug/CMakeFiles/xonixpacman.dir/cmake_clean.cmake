b17df6684ad8426c0b4e1ab31b5eece9131c9e6e
Jonathan Prieto <jonathan.prieto@me.com>
2018-11-21 12:24:20 -0500

    0.2
    CMAKE FILES OK

---
+++ cmake-build-debug/CMakeFiles/xonixpacman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xonixpacman.dir/main.cpp.o"
+  "xonixpacman.pdb"
+  "xonixpacman"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xonixpacman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
